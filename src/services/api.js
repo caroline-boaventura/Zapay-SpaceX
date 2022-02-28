8cc0b414567e0eec33b2fd83ec647c24cc0dbfb8
Caroline <caroline.boaventura08@gmail.com>
2022-02-28 10:17:35 -0300

    feat: development of getLatestLaunch() function

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,22 @@
 import axios from 'axios';
 
+const api = axios.create({
+    baseURL: 'https://api.spacexdata.com',
+})
+
 export const getNextLaunch = () => {
-    const nextLaunch = axios.get('https://api.spacexdata.com/v4/launches/next')
+    const nextLaunch = api.get('/v4/launches/next')
         .then((response) => response.data)
         .catch((error) => console.log(error.response));
     
     return nextLaunch;
+};
+
+export const getLatestLaunch = async () => {
+    const latestLaunch = await api.get('/v4/launches/latest')
+        .then((response) => response.data)
+        .catch((error) => console.log(error.response))
+
+    return latestLaunch; 
 }
+
