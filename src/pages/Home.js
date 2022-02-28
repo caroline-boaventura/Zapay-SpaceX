90c46a9736274d39af14ed41031866e85586d70f
Caroline <caroline.boaventura08@gmail.com>
2022-02-28 10:48:14 -0300

    feat: implements onClick functions on the Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,40 @@
 import React, { useState } from 'react';
+import { getNextLaunch, getLatestLaunch, getAllUpcomingLaunches, getAllPastLaunches } from '../services/api';
 import Header from '../components/Header';
 import Button from '../components/Button';
 import Body from '../components/Body';
 
 function Home() {
+    const [data, setData] = useState({});
 
+    const nextLaunch = async () => {
+        const response = await getNextLaunch();
+        setData(response);
+    }
+
+    const latestLaunch = async () => {
+        const response = await getLatestLaunch();
+        setData(response);
+    }
+
+    const allUpComingLaunches = async () => {
+        const response = await getAllUpcomingLaunches();
+        setData(response);
+    }
+
+    const allPastLaunches = async () => {
+        const response = await getAllPastLaunches();
+        setData(response);
+    }
 
     return (
       <div>
         <Header />
-        <Button name='Próximo Lançamento' />
-        <Button name='Último Lançamento' />
-        <Button name='Próximos Lançamentos' />
-        <Button name='Lançamentos passados' />
-        <Body />
+        <Button name='Próximo Lançamento' func={ nextLaunch } />
+        <Button name='Último Lançamento' func={ latestLaunch } />
+        <Button name='Próximos Lançamentos' func={ allUpComingLaunches } />
+        <Button name='Lançamentos passados' func={ allPastLaunches } />
+        <Body data={ data } />
       </div>
     );
   }
