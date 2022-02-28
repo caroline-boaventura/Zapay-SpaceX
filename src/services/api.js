365944ddd019d89ce059059ee6d70813db67deab
Caroline <caroline.boaventura08@gmail.com>
2022-02-28 10:19:39 -0300

    feat: development of getAllUpcomingLaunches() function

--- src/services/api.js
+++ src/services/api.js
@@ -12,11 +12,18 @@ export const getNextLaunch = () => {
     return nextLaunch;
 };
 
-export const getLatestLaunch = async () => {
-    const latestLaunch = await api.get('/v4/launches/latest')
+export const getLatestLaunch = () => {
+    const latestLaunch = api.get('/v4/launches/latest')
         .then((response) => response.data)
-        .catch((error) => console.log(error.response))
+        .catch((error) => console.log(error.response));
 
     return latestLaunch; 
 }
 
+export const getAllUpcomingLaunches = () => {
+    const allUpcomingLaunches = api.get('v4/launches/upcoming')
+        .then((response) => response.data)
+        .catch((error) => console.log(error.response));
+    
+    return allUpcomingLaunches;
+}
